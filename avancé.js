30852d679ff58045423fb8fc7419fd62a2b7ae49
akosy <develllopeurweb@gmail.com>
2022-12-22 16:25:13 +0100

    ajout des exo avancé

---
+++ avancé.js
@@ -0,0 +1,15 @@
+// definir la liste de couleur dans le tableau 
+let array = ['red', 'green', 'blue', 'yellow', 'orange']
+
+// parcourir la list dans le tableau
+for (let i = 0; i < array.length; i++) {
+   console.log(array[i])}
+
+// definir la liste du tableau 
+let array = ['1', '2', '3', '4', '5']
+array.push(6)
+console.log(array)
+array.pop(5)
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
