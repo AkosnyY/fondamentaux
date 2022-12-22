4ababede6f9f1f52f066a55ca5d335f7ca71ef28
akosy <develllopeurweb@gmail.com>
2022-12-22 16:27:27 +0100

    Ajout des exos conténation

---
+++ conténation.js
@@ -0,0 +1,11 @@
+// let déclare des variales  
+let [fristName] = ["John"]
+let [lastName ] = ["Doe "]
+
+//renvoie une chaine
+typeof(fristName)
+
+let complete = `${fristName} ${lastName}`
+
+// affiche un message de son nom et prénom 
+console.log(`John Doe`)
