c757cbd2fd585263a96194b82747a31e3bfb3067
akosy <develllopeurweb@gmail.com>
2022-12-22 16:26:20 +0100

    Ajout des exos conditions

---
+++ conditions.js
@@ -0,0 +1,14 @@
+// definir une variables 
+let age = 19;
+// conditionelle if 
+// analyser si l'age et égal ou plus grand que 
+if (age >18){
+    console.log("Vous êtes majeur")
+}
+// conditionnelle else 
+// analyser si l'age et plus que 
+else{
+    console.log("Vous êtes mineur")
+}
+
+
